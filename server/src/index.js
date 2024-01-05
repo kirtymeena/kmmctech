9d5643ff311cb983862c9bf4ced1a39a6bc3225a
kirtymeena <kirtymeena@gmail.com>
2024-01-05 13:51:54 +0530

    deployment files added

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express();
+const router = require("express").Router();
+
 const cors = require("cors");
 require("./db/connection")
 const navigation = require("./routes/navigation");
@@ -7,8 +9,14 @@ const homeCarousel = require("./routes/homeCarousel");
 const products = require("./routes/products");
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin:'*',
+    method:['POST','GET']
+}))
 app.use(express.json())
+app.get("/",(req,res)=>{
+   res.json("server's up")
+})
 app.use("/api/v1",navigation);
 app.use("/api/v1",homeCarousel);
 app.use("/api/v1",products);
